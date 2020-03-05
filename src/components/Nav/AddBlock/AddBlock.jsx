51281592cecc31f9c86db528cb27035ed4e5f2f3
lnkz80 <lnkz80@gmail.com>
2020-03-05 23:01:07 +0200

    new component BlockItem

--- src/components/Nav/AddBlock/AddBlock.jsx
+++ src/components/Nav/AddBlock/AddBlock.jsx
@@ -1,35 +1,24 @@
 import React from 'react';
 import classes from './AddBlock.module.css';
 
-// const usrItemAddBlock = () => {
-//   return (
-//     <div>
-//       <img src='./img/usr_avatar.jpg' />
-//       <span>Name</span>
-//     </div>
-//   );
-// };
+const BlockItem = () => {
+  return (
+    <div className={classes.usrItem}>
+      <img src='./img/usr_avatar.jpg' />
+      <span>Name</span>
+    </div>
+  );
+};
 
 const AddBlock = props => {
   return (
     <div className={classes.box}>
       <h5>{props.head}</h5>
-      <div className={classes.usrItem}>
-        <img src='./img/usr_avatar.jpg' />
-        <span>Name</span>
-      </div>
-      <div className={classes.usrItem}>
-        <img src='./img/usr_avatar.jpg' />
-        <span>Name</span>
-      </div>
-      <div className={classes.usrItem}>
-        <img src='./img/usr_avatar.jpg' />
-        <span>Name</span>
-      </div>
-      <div className={classes.usrItem}>
-        <img src='./img/usr_avatar.jpg' />
-        <span>Name</span>
-      </div>
+      <BlockItem />
+      <BlockItem />
+      <BlockItem />
+      <BlockItem />
+      <BlockItem />
     </div>
   );
 };
